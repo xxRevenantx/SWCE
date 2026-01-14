7603b2f83d50f27372ca3300bec2596b2d41c374
xxRevenantx <beto_war_157@hotmail.com>
2026-01-13 23:31:49 -0600

    feat: :fire: Rutas

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,19 +21,19 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    // public function definition(): array
-    // {
-    //     // return [
-    //     //     'username' => fake()->unique()->userName(),
-    //     //     'email' => fake()->unique()->safeEmail(),
-    //     //     'email_verified_at' => now(),
-    //     //     'password' => static::$password ??= Hash::make('password'),
-    //     //     'remember_token' => Str::random(10),
-    //     //     'two_factor_secret' => Str::random(10),
-    //     //     'two_factor_recovery_codes' => Str::random(10),
-    //     //     'two_factor_confirmed_at' => now(),
-    //     // ];
-    // }
+    public function definition(): array
+    {
+        // return [
+        //     'username' => fake()->unique()->userName(),
+        //     'email' => fake()->unique()->safeEmail(),
+        //     'email_verified_at' => now(),
+        //     'password' => static::$password ??= Hash::make('password'),
+        //     'remember_token' => Str::random(10),
+        //     'two_factor_secret' => Str::random(10),
+        //     'two_factor_recovery_codes' => Str::random(10),
+        //     'two_factor_confirmed_at' => now(),
+        // ];
+    }
 
     /**
      * Indicate that the model's email address should be unverified.
