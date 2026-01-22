53bb4a8d3f7cf625328ac22628a4cbc3e22c4062
xxRevenantx <beto_war_157@hotmail.com>
2026-01-22 13:08:34 -0600

    feat: :art: Inscripciones

---
+++ database/migrations/2026_01_22_120751_create_inscripciones_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inscripciones', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('alumno_id');
+            $table->unsignedBigInteger('licenciatura_id');
+            $table->unsignedBigInteger('generacion_id');
+            $table->unsignedBigInteger('cuatrimestre_id');
+            $table->boolean('status')->default(true);
+            $table->date('fecha_inscripcion');
+
+            $table->foreign('alumno_id')->references('id')->on('alumnos')->onDelete('cascade');
+            $table->foreign('licenciatura_id')->references('id')->on('licenciaturas')->onDelete('cascade');
+            $table->foreign('generacion_id')->references('id')->on('generaciones')->onDelete('cascade');
+            $table->foreign('cuatrimestre_id')->references('id')->on('cuatrimestres')->onDelete('cascade');
+
+
+
+            $table->unique(
+                ['alumno_id', 'licenciatura_id', 'generacion_id', 'cuatrimestre_id'],
+                'inscripciones_unique_alumno_lic_gener_cuat'
+            );
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inscripciones');
+    }
+};
