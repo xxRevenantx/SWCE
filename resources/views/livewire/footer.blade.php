807a6dae6710726f79e09dcef505503601dd2477
xxRevenantx <beto_war_157@hotmail.com>
2026-02-24 09:53:34 -0600

    Calificaciones

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full  pt-4 ">
         <div class="sm:flex sm:items-center ">
             <span class="text-sm text-body sm:text-center">© {{ date('Y') }} Sistema Web de Control Escolar. Todos
-                los derechos reservados.
+                los derechos reservados
             </span>
         </div>
     </div>
